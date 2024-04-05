8ff2eea77321ed6bc8f3733d6c92589aa7936c0f
Mark-Haze <markhazen05@hotmail.com>
2024-04-05 11:56:28 -0500

    Updated LED implementation

--- Lab3/Vivado/Lab1/Lab1.srcs/sources_1/new/top_demo.sv
+++ Lab3/Vivado/Lab1/Lab1.srcs/sources_1/new/top_demo.sv
@@ -64,12 +64,8 @@ module top_demo
 
   fsm state(smol_clk, btn[1], sw[1], sw[0], y, ledOut);
 
-  assign led[0] = ledOut[0];
-  assign led[1] = ledOut[1];
-  assign led[2] = ledOut[2];
-  assign led[5] = ledOut[3];
-  assign led[6] = ledOut[4];
-  assign led[7] = ledOut[5];
+  assign led[2:0] = ledOut[2:0];
+  assign led[7:5] = ledOut[5:3];
 
 // Register logic storing clock counts
   always@(posedge sysclk_125mhz)
